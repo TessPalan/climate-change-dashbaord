8f074cb7476ca72c708af26b53624686f0897863
Ellis Mok <ellismok@Elliss-MBP.home>
2021-05-01 13:45:27 -0700

    add files

--- visualization/PyTrend_d3/pytrends_d3_line_app/js/logic.js
+++ visualization/PyTrend_d3/pytrends_d3_line_app/js/logic.js
@@ -1,9 +1,15 @@
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+// Set the dimensions and margins of the graph
+var margin = { top: 30, right: 120, bottom: 50, left: 50 },
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom,
+    tooltip = { width: 100, height: 100, x: 10, y: -30 };
 
-// append the svg object to the body of the page
+// smaller version! 
+// var margin = {top: 50, right: 30, bottom: 70, left: 100},
+//     width = 460 - margin.left - margin.right,
+//     height = 400 - margin.top - margin.bottom;
+
+// Append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -12,31 +18,36 @@ var svg = d3.select("#my_dataviz")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
+// Append the svg object to the body of the page
+svg.append("text")
+  .attr("x", (width / 2))             
+  .attr("y", 0 - (margin.top / 2))
+  .attr("text-anchor", "middle")  
+  .style("font-size", "16px") 
+  .style("text-decoration", "underline")  
+  .text("Pytrend Key Words Search Volume (2004 to 2016)");
+
 // var key = 0; --> testing empty dataset
 
-//Read the data
+// Read the data
 d3.csv("../../../raw data/keyword_search_volume_US.csv").then(function(data) {
-  // console.log(data);
-    // List of groups (here I have one group per column)
+    // console.log(data);
     // key = data --> testing as well 
-    var allGroup = Object.keys(data[0]).slice(1)
+    var allGroup = Object.keys(data[0]).slice(1) // this function gets rid of an element from a list
 
+    // Convert the year to only the year
     var timeConv = data.map(data => {var year = d3.timeParse("%Y")(data.date);
     data["date"] = year;
     return data})
-    // Note: Timestamp to rec int as year 
-    // var timeConv = data.map(data => {var year = +data.date;
-    //                data["date"] = year;
-    //                return data})
-    console.log(timeConv);
+    // console.log(timeConv);
                    
-    // add the options to the button
+    // Add the options to the button
     d3.select("#selectButton")
       .selectAll('myOptions')
      	.data(allGroup)
       .enter()
     	.append('option')
-      .text(function (d) { return d; }) // text showed in the menu
+      .text(function (d) { return d; }) // d signifies the element in the data
       .attr("value", function (d) { return d; }) // corresponding value returned by the button
 
     // A color scale: one color for each group
@@ -44,20 +55,35 @@ d3.csv("../../../raw data/keyword_search_volume_US.csv").then(function(data) {
       .domain(allGroup)
       .range(d3.schemeSet2);
 
-    // Add X axis --> it is a date format: d3.timeParse("%Y")(data.date)
+    // Add X axis --> it is a date format
     var x = d3.scaleTime()
       .domain(d3.extent(data, function(d) { return d.date; }))
       .range([ 0, width ]);
     svg.append("g")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x).ticks(7));
+      .call(d3.axisBottom(x).ticks(7).tickSizeOuter(0));
+    // text label for the x axis
+    svg.append("text")             
+      .attr("transform",
+            "translate(" + (width/2) + " ," + 
+            (height + margin.top + 20) + ")")
+      .style("text-anchor", "middle")
+      .text("Years");
 
-    // Add Y axis
+    // Add Y axis --> keywords search volume
     var y = d3.scaleLinear()
       .domain([0, d3.max(data, function(d) { return +d["Clean Energy"]; })])
       .range([ height, 0 ]);
     var yaxis = svg.append("g")
       .call(d3.axisLeft(y));
+    // text label for the y axis
+    svg.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .text("Volume of Keywords Searched");  
 
     // Initialize line with first group of the list
     var line = svg
@@ -94,6 +120,7 @@ d3.csv("../../../raw data/keyword_search_volume_US.csv").then(function(data) {
           )
           .attr("stroke", function(d){ return myColor(selectedOption) })
     }
+    
 
     // When the button is changed, run the updateChart function
     d3.select("#selectButton").on("change", function(d) {
@@ -104,3 +131,4 @@ d3.csv("../../../raw data/keyword_search_volume_US.csv").then(function(data) {
     })
 
 })
+
